b30a71b90c774625ae8ee683c1bcc1b3d783a11b
divyani-joshi <divyanijoshi577@gmail.com>
2026-06-22 16:55:01 +0530

    Updated cors

--- hir_backend/index.js
+++ hir_backend/index.js
@@ -44,7 +44,7 @@ app.use(cors({
     "http://localhost:3001",
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://hir-atelier.vercel.app/service"
+    "https://hir-atelier.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
